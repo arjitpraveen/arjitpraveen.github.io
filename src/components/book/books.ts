3d14db2920579261eb40b749b700eea1c8a35bfe
Arjit Praveen <arjitpraveen.sec@proton.me>
2026-01-29 14:03:52 +0530

    resolve errors

--- src/components/book/books.ts
+++ src/components/book/books.ts
@@ -1,4 +1,8 @@
-export type BookStatus = 'reading' | 'to-read' | 'finished' | 'dropped'
+export type BookStatus =
+  | 'reading'
+  | 'to-read'
+  | 'finished'
+  | 'dropped'
 
 export type Book = {
   title: string
@@ -7,52 +11,43 @@ export type Book = {
   status: BookStatus
 }
 
-export const books = {
+export const books: {
+  academic: Book[]
+  fiction: Book[]
+} = {
   academic: [
     {
-      title: 'Introduction to Algorithms',
-      author: 'Cormen, Leiserson, Rivest, Stein',
-      cover: 'https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=759&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      status: 'reading',
+      title: "Introduction to Algorithms",
+      author: "Cormen, Leiserson, Rivest, Stein",
+      cover: "https://images-na.ssl-images-amazon.com/images/I/41as+WafrFL._SX258_BO1,204,203,200_.jpg",
+      status: "reading",
     },
     {
-      title: 'Operating Systems: Three Easy Pieces',
-      author: 'Remzi H. Arpaci-Dusseau',
-      cover: 'https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=759&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      status: 'to-read',
+      title: "Operating Systems: Three Easy Pieces",
+      author: "Remzi H. Arpaci-Dusseau & Andrea C. Arpaci-Dusseau",
+      cover: "https://imgs.search.brave.com/k9mnyUM7norSlMVWawjCbH9mq6I-2j_ooZ0V-fuFlB0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NTFsVHNELUxHb0wu/anBn",
+      status: "to-read",
     },
     {
-      title: 'Computer Networking: A Top-Down Approach',
-      author: 'Kurose & Ross',
-      cover: 'https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=759&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      status: 'finished',
-    },
-    {
-      title: 'Artificial Intelligence: A Modern Approach',
-      author: 'Russell & Norvig',
-      cover: 'https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=759&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      status: 'dropped',
+      title: "Computer Networking: A Top-Down Approach",
+      author: "Kurose & Ross",
+      cover: "https://images-na.ssl-images-amazon.com/images/I/51x5NY1kE+L._SX396_BO1,204,203,200_.jpg",
+      status: "finished",
     },
   ],
 
   fiction: [
     {
-      title: 'Dune',
-      author: 'Frank Herbert',
-      cover: 'https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=759&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      status: 'reading',
+      title: "1984",
+      author: "George Orwell",
+      cover: "https://images-na.ssl-images-amazon.com/images/I/41KD2-7-PNL._SX324_BO1,204,203,200_.jpg",
+      status: "finished",
     },
     {
-      title: '1984',
-      author: 'George Orwell',
-      cover: 'https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=759&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      status: 'finished',
-    },
-    {
-      title: 'Neuromancer',
-      author: 'William Gibson',
-      cover: 'https://images.unsplash.com/photo-1484589065579-248aad0d8b13?q=80&w=759&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      status: 'to-read',
+      title: "Dune",
+      author: "Frank Herbert",
+      cover: "https://images-na.ssl-images-amazon.com/images/I/51aEhwAtYaL._SX320_BO1,204,203,200_.jpg",
+      status: "to-read",
     },
   ],
 }
