828d82ffb670e47f4921714d2b8c245c99baad71
Arjit Praveen <arjitpraveen.sec@proton.me>
2026-02-02 14:55:15 +0530

    update site url

--- astro.config.ts
+++ astro.config.ts
@@ -20,7 +20,7 @@ import type { ExpressiveCodeTheme } from 'rehype-expressive-code'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://arjitpraveen.github.io',
+  site: 'https://chr0mazone.github.io',
   integrations: [mdx(), react(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
